cd00c80f13ef8ed7740cc9e7b259a61cd38c318e
Guilherme Ozaki <guilherme02almeida@hotmail.com>
2026-09-21 11:24:31 -0300

    chore: inicia projeto Spring Boot com Java 21

---
+++ src/main/java/com/portfolio/repasse/RepasseApiApplication.java
@@ -0,0 +1,14 @@
+package com.portfolio.repasse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepasseApiApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/portfolio/repasse/RepasseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.repasse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepasseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
