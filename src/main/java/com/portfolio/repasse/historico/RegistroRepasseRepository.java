0f60631888544b843c8d59d86c4567ed4966fce2
Guilherme Ozaki <guilherme02almeida@hotmail.com>
2026-09-25 09:00:35 -0300

    feat: adiciona repositório JPA do histórico de repasses

---
+++ src/main/java/com/portfolio/repasse/historico/RegistroRepasseRepository.java
@@ -0,0 +1,7 @@
+package com.portfolio.repasse.historico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegistroRepasseRepository extends JpaRepository<RegistroRepasse, Long> {
+
+}
